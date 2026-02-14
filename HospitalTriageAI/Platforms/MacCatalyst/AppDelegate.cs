96bc2f801190ca8e3ec9041b11cb873769f213d6
muthaiah-ARK <128181021@sastra.ac.in>
2026-02-14 16:03:00 +0530

    Enhanced UI

--- HospitalTriageAI/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace HospitalTriageAI;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
